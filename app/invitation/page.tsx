475725756950dc24aa1d8abee2953184321df423
eang010 <3mily.ang@gmail.com>
2025-02-17 23:55:14 +0800

    Update delay duration to scroll

--- app/invitation/page.tsx
+++ app/invitation/page.tsx
@@ -51,7 +51,7 @@ export default function Invitation() {
               animate={{
                 height: "auto",
                 opacity: 1,
-                transition: { duration: 0.3 }
+                transition: { duration: 0.5 }
               }}
               exit={{ height: 0, opacity: 0 }}
               onAnimationComplete={() => {
